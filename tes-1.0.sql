25fcfdcf68bc7f72e701dfcdb80d1cdc6576e506
Sawtooth04 <andrey.y96@mail.ru>
2023-08-27 14:37:04 +0300

    Added roles table with init function. Changed customers table for reference to roles.

--- tes-1.0.sql
+++ tes-1.0.sql
@@ -12,7 +12,7 @@
  Target Server Version : 150001
  File Encoding         : 65001
 
- Date: 23/08/2023 18:40:46
+ Date: 27/08/2023 14:36:03
 */
 
 
@@ -28,6 +28,17 @@ START 1
 CACHE 1;
 
 -- ----------------------------
+-- Sequence structure for Role_roleID_seq
+-- ----------------------------
+DROP SEQUENCE IF EXISTS "public"."Role_roleID_seq";
+CREATE SEQUENCE "public"."Role_roleID_seq" 
+INCREMENT 1
+MINVALUE  1
+MAXVALUE 2147483647
+START 1
+CACHE 1;
+
+-- ----------------------------
 -- Table structure for Customer
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."Customer";
@@ -35,13 +46,31 @@ CREATE TABLE "public"."Customer" (
   "customerID" int4 NOT NULL DEFAULT nextval('"Customer_customerID_seq"'::regclass),
   "name" varchar(30) COLLATE "pg_catalog"."default",
   "passwordHash" varchar(255) COLLATE "pg_catalog"."default",
-  "email" varchar(50) COLLATE "pg_catalog"."default"
+  "email" varchar(50) COLLATE "pg_catalog"."default",
+  "roleID" int4
 )
 ;
 
 -- ----------------------------
 -- Records of Customer
 -- ----------------------------
+INSERT INTO "public"."Customer" VALUES (1, 'Sawtooth', 'test', 'test', 1);
+
+-- ----------------------------
+-- Table structure for Role
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."Role";
+CREATE TABLE "public"."Role" (
+  "roleID" int4 NOT NULL DEFAULT nextval('"Role_roleID_seq"'::regclass),
+  "name" varchar(30) COLLATE "pg_catalog"."default"
+)
+;
+
+-- ----------------------------
+-- Records of Role
+-- ----------------------------
+INSERT INTO "public"."Role" VALUES (1, 'user');
+INSERT INTO "public"."Role" VALUES (2, 'admin');
 
 -- ----------------------------
 -- Function structure for create_customer_table
@@ -54,7 +83,10 @@ CREATE OR REPLACE FUNCTION "public"."create_customer_table"()
 		"customerID" serial PRIMARY KEY,
 		name varchar(30),
 		"passwordHash" varchar(255),
-		email varchar(50)
+		email varchar(50),
+		"roleID" int4,
+		
+		FOREIGN KEY ("roleID") REFERENCES "Role" ("roleID") ON UPDATE CASCADE ON DELETE RESTRICT
 	);
 	
 	RETURN;
@@ -63,18 +95,21 @@ END$BODY$
   COST 100;
 
 -- ----------------------------
--- Function structure for get_customer_by_id
+-- Function structure for create_role_table
 -- ----------------------------
-DROP FUNCTION IF EXISTS "public"."get_customer_by_id"("id" int4);
-CREATE OR REPLACE FUNCTION "public"."get_customer_by_id"("id" int4)
-  RETURNS SETOF "public"."Customer" AS $BODY$BEGIN
+DROP FUNCTION IF EXISTS "public"."create_role_table"();
+CREATE OR REPLACE FUNCTION "public"."create_role_table"()
+  RETURNS "pg_catalog"."void" AS $BODY$BEGIN
 	
-	RETURN QUERY SELECT * FROM "Customer" WHERE "Customer"."customerID" = "id";
+	CREATE TABLE "Role" (
+		"roleID" serial PRIMARY KEY,
+		name varchar(30)
+	);
 
+	RETURN;
 END$BODY$
   LANGUAGE plpgsql VOLATILE
-  COST 100
-  ROWS 1000;
+  COST 100;
 
 -- ----------------------------
 -- Function structure for insert_customer
@@ -91,6 +126,39 @@ END$BODY$
   COST 100;
 
 -- ----------------------------
+-- Function structure for sec_get_auth
+-- ----------------------------
+DROP FUNCTION IF EXISTS "public"."sec_get_auth"("username" varchar);
+CREATE OR REPLACE FUNCTION "public"."sec_get_auth"("username" varchar)
+  RETURNS TABLE("name" varchar, "role" varchar) AS $BODY$BEGIN
+
+	RETURN QUERY
+		SELECT "Customer".name, r.name FROM "Customer"
+			LEFT JOIN "Role" AS r ON "Customer"."roleID" = r."roleID"
+			WHERE "Customer".name = username;
+			
+END$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100
+  ROWS 1000;
+
+-- ----------------------------
+-- Function structure for sec_get_user
+-- ----------------------------
+DROP FUNCTION IF EXISTS "public"."sec_get_user"("username" varchar);
+CREATE OR REPLACE FUNCTION "public"."sec_get_user"("username" varchar)
+  RETURNS TABLE("name" varchar, "password" varchar, "enabled" bool) AS $BODY$BEGIN
+
+	RETURN QUERY
+		SELECT "Customer".name, "passwordHash", TRUE FROM "Customer"
+			WHERE "Customer".name = username;
+		
+END$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100
+  ROWS 1000;
+
+-- ----------------------------
 -- Alter sequences owned by
 -- ----------------------------
 ALTER SEQUENCE "public"."Customer_customerID_seq"
@@ -98,6 +166,23 @@ OWNED BY "public"."Customer"."customerID";
 SELECT setval('"public"."Customer_customerID_seq"', 2, true);
 
 -- ----------------------------
+-- Alter sequences owned by
+-- ----------------------------
+ALTER SEQUENCE "public"."Role_roleID_seq"
+OWNED BY "public"."Role"."roleID";
+SELECT setval('"public"."Role_roleID_seq"', 3, true);
+
+-- ----------------------------
 -- Primary Key structure for table Customer
 -- ----------------------------
 ALTER TABLE "public"."Customer" ADD CONSTRAINT "Customer_pkey" PRIMARY KEY ("customerID");
+
+-- ----------------------------
+-- Primary Key structure for table Role
+-- ----------------------------
+ALTER TABLE "public"."Role" ADD CONSTRAINT "Role_pkey" PRIMARY KEY ("roleID");
+
+-- ----------------------------
+-- Foreign Keys structure for table Customer
+-- ----------------------------
+ALTER TABLE "public"."Customer" ADD CONSTRAINT "Customer_roleID_fkey" FOREIGN KEY ("roleID") REFERENCES "public"."Role" ("roleID") ON DELETE RESTRICT ON UPDATE CASCADE;
